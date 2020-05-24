99cbc435a1c041db19829c789fe7b112615a14e2
hao shan <haoshan9898@gmail.com>
2020-05-24 20:41:09 +0800

    2 Player created, move arrows by mouse click

---
+++ src/numerical_bow/Arrow.java
@@ -0,0 +1,71 @@
+package numerical_bow;
+
+import java.awt.*;
+
+public class Arrow {
+
+    private Point arrowLoc;
+    private int arrowL = 35;
+    private int arrowW = 2;
+    private Graphics g;
+    private boolean isToRight;
+
+    private double angle;
+    private double power;
+    private double accelaration;
+    private double gravity = 9.81;
+
+    int[] xPoints;
+    int[] yPoints;
+
+    public Arrow(Graphics g, Point loc, boolean isToRight) {
+        this.g = g;
+        this.isToRight = isToRight;
+        this.arrowLoc = loc;
+
+        //set arrow facing side
+        if (isToRight) {
+            xPoints = new int[]{2, 0, 1, 3, 8, 8, 10, 8, 8, 3, 1, 0};
+            yPoints = new int[]{6, 8, 9, 7, 7, 8, 6, 4, 5, 5, 3, 4};
+
+        } else {
+            xPoints = new int[]{0, 2, 2, 7, 9, 10, 8, 10, 9, 7, 2, 2};
+            yPoints = new int[]{6, 8, 7, 7, 9, 8, 6, 4, 3, 5, 5, 4};
+
+        }
+
+        // set arrow size
+        for (int i = 0; i < xPoints.length; i++) {
+            if (xPoints[i] > 6) {
+                xPoints[i] += arrowL;
+            }
+            if (yPoints[i] < 6) {
+                yPoints[i] -= arrowW / 2;
+            } else if (yPoints[i] > 6) {
+                yPoints[i] += arrowW / 2;
+            }
+        }
+
+        // set arrow to its location
+        for (int i = 0; i < xPoints.length; i++) {
+            xPoints[i] += loc.x;
+        }
+        for (int i = 0; i < yPoints.length; i++) {
+            yPoints[i] += loc.y;
+        }
+
+        this.arrowLoc = loc;
+
+        drawArrow();
+    }
+
+    public Point getArrowLoc() {
+        return arrowLoc;
+    }
+
+
+    public void drawArrow() {
+        g.drawPolygon(xPoints, yPoints, xPoints.length);
+    }
+
+}
---
+++ src/numerical_bow/NumericalBow.java
@@ -0,0 +1,226 @@
+package numerical_bow;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+// http://greenteapress.com/thinkjava6/html/thinkjava6017.html
+// https://www.youtube.com/watch?v=pDafZdIIeNE
+/**
+ * 1) Once the game is start, both player stand at their position, with bow and
+ * arrow
+ *
+ * 2) When mouse click is pointed near the particular player, the dragging(X)
+ * control power, dragging(Y) control direction(angle).
+ *
+ * 3) When mouse click released, arrow is being shoot with certain acceleration.
+ *
+ *
+ * --later when we make 2 players far apart, our scene should follow the flying
+ * of the arrow.
+ *
+ * 4) Two condition for the released arrow:
+ *
+ * --4a) successfully attacked opponent
+ *
+ * ----decrease opponent life, if less than 0, died, game over
+ *
+ * --4b) drop to floor (then change arrow to another color)
+ *
+ * 5) Once the shooting is done, created new arrow for player
+ *
+ * 6) The scene will stop at the arrow stop point (either land or another
+ * player)
+ *
+ * --We use scroll bar(controlled by keyboard?) to find another player
+ *
+ * 7) Another player start to shoot.
+ */
+public class NumericalBow extends JPanel {
+
+    private Image img;
+    private Point lastPoint;
+    private final Point p1;  //player 1
+    private final Point p2;  //player 2
+    private Arrow[] arrow = new Arrow[2];
+    private Point[] arrowLoc = new Point[2];
+    private boolean[] isReleased = {false, false};
+    private boolean[] init = {true, true};
+    private boolean[] toMove = {false, false};
+
+    public NumericalBow() {
+
+        //Layout
+        setBackground(new Color(0xFFEBCD));
+
+//        addMouseMotionListener(new MouseMotionAdapter() {
+//            public void mouseDragged(MouseEvent e) {
+//                Graphics g = getGraphics();
+//                g.drawLine(lastPoint.x, lastPoint.y, e.getX(), e.getY());
+//                g.dispose();
+//            }
+//        });
+        this.p1 = new Point(100, 100);
+        this.p2 = new Point(700, 100);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getX() < 450) {
+                    System.out.println("\nMouse press on left");
+                    init[0] = false;
+                    isReleased[0] = true;
+                    toMove[0] = true;
+                    toMove[1] = false;
+                } else {
+                    System.out.println("\nMouse press on right");
+                    init[1] = false;
+                    isReleased[1] = true;
+                    toMove[1] = true;
+                    toMove[0] = false;
+                }
+                repaint();
+            }
+        });
+
+//        addKeyListener(new KeyAdapter() {
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//                switch (e.getKeyCode()) {
+//                    case KeyEvent.VK_LEFT:
+//                        if (!END) {
+//                            moveLeft();
+//                        }
+//                        break;
+//                    case KeyEvent.VK_RIGHT:
+//                        if (!END) {
+//                            moveRight();
+//                        }
+//                        break;
+//                    case KeyEvent.VK_U:
+//                        if (!END) {
+//                            undo();
+//                        }
+//                        break;
+//                }
+//                repaint();
+//            }
+//        });
+    }
+
+    @Override
+    public void paintComponent(Graphics gg) {
+        super.paintComponent(gg);
+        Graphics2D g = (Graphics2D) gg;
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+
+        createPlayer(g, p1, true);
+        createPlayer(g, p2, false);
+
+        createLand(g);
+
+        lifeBar(g);
+
+    }
+
+    public void createPlayer(Graphics g, Point p, boolean isFaceRight) {
+        int height = 100;
+        int width = 20;
+
+        //body
+        g.fillOval(p.x, p.y, width, height / 2);
+        //head
+        g.fillOval(p.x, p.y - width, width, width);
+        //leg
+        g.fillOval(p.x, p.y + height / 2, height / width * 2, height / 2 - width);
+        g.fillOval(p.x + width / 2, p.y + height / 2, height / width * 2, height / 2 - width);
+
+        //hand
+        g.fillOval(p.x - width - 10, p.y + width, height / 2 - width, height / width + 2);
+        g.fillOval(p.x + width, p.y + width, height / 2 - width, height / width + 2);
+
+        //TODO: Movable hand & bow, link with arrow
+        //TODO: Arrow dragging (control both power & angle)
+        //bow
+        //arrow
+        if (init[0] & !isReleased[0] & isFaceRight) {
+            arrowLoc[0] = new Point(p.x + 20, p.y);
+            arrow[0] = new Arrow(g, arrowLoc[0], isFaceRight);
+
+            System.out.println("Arrow 0 created");
+        } else if (init[1] & !isReleased[1] & !isFaceRight) {
+            arrowLoc[1] = new Point(p.x - 50, p.y);
+            arrow[1] = new Arrow(g, arrowLoc[1], isFaceRight);
+
+            System.out.println("Arrow 1 created");
+        }
+
+        if (isReleased[0] & isFaceRight) {
+            if (toMove[0]) {
+                arrow[0] = move(g, arrowLoc[0], 20, isFaceRight);
+                System.out.println("Arrow 0 : " + Arrays.toString(arrow[0].xPoints));
+            } else {
+                arrow[0] = move(g, arrowLoc[0], 0, isFaceRight);
+            }
+        } else if (isReleased[1] & !isFaceRight) {
+            if (toMove[1]) {
+                arrow[1] = move(g, arrowLoc[1], 20, isFaceRight);
+                System.out.println("Arrow 1 : " + Arrays.toString(arrow[1].xPoints));
+
+            } else {
+                arrow[1] = move(g, arrowLoc[1], 0, isFaceRight);
+            }
+
+        }
+
+    }
+
+    public void createLand(Graphics g) {
+
+        g.drawLine(0, 300, 900, 300);
+    }
+
+    //TODO: lifeBar update
+    public void lifeBar(Graphics g) {
+        //lifebar
+
+        //power
+        g.setFont(new Font("SansSerif", Font.BOLD, 15));
+        g.drawString("Power : 10", 100, 50);
+        g.setFont(new Font("SansSerif", Font.BOLD, 15));
+        g.drawString("Power : 10", 700, 50);
+
+        //angle
+        g.setFont(new Font("SansSerif", Font.BOLD, 15));
+        g.drawString("Angle : 0", 100, 60);
+        g.setFont(new Font("SansSerif", Font.BOLD, 15));
+        g.drawString("Angle : 0", 700, 60);
+    }
+
+    //TODO: arrow movement (acceleration, decceleration), rotation
+    //TODO: scene follow arrow position
+    public Arrow move(Graphics g, Point arrowPoint, int velocity, boolean isFaceRight) {
+
+        if (isFaceRight) {
+            arrowPoint.x += velocity;
+        } else {
+            arrowPoint.x -= velocity;
+        }
+        return new Arrow(g, arrowPoint, isFaceRight);
+    }
+
+    //TODO: scene scrolling (keyboard)
+    public void sceneScroll() {
+
+    }
+
+    //TODO: maintain dropped arrow, change color
+    //
+}
